02b6f786df60b5d0b83791a4721441ecbb58f1e8
Ryan Oliveira <contato.ryanoliveira@outlook.com>
2022-05-18 21:49:04 -0300

    setup: init

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export function App() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App
